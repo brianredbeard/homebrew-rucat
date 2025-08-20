ff74b295a869617a5c5d917b260de49a881fd906
Brian 'redbeard' Harrington <redbeard@dead-city.org>
2025-08-19 18:27:42 -0700

    update sha256 hash

--- Formula/rucat.rb
+++ Formula/rucat.rb
@@ -3,7 +3,7 @@ class Rucat < Formula
   homepage "https://github.com/brianredbeard/rucat"
   url "https://github.com/brianredbeard/rucat/archive/refs/tags/v0.2.0.tar.gz"
   version "0.2.0"
-  sha256 "5a317789196b0527339790b021d7b14d8622c71f54d32a2491b402120e5c1285"
+  sha256 "46fba4db72cea12bfc339ab35a02c2f5e643493a9006200ec68c9ac5fd406e84"
   license "GPL-3.0-or-later"
   head "https://github.com/brianredbeard/rucat.git", branch: "main"
 
