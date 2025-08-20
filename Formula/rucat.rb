7a76221e200cb2eefb7927efb8fe718ca8832383
Brian 'redbeard' Harrington <redbeard@dead-city.org>
2025-08-19 18:06:07 -0700

    initial template commit

---
+++ Formula/rucat.rb
@@ -0,0 +1,16 @@
+class Rucat < Formula
+  desc "rucat is cat reborn for the era of LLMs. A critical tool for every prompt engineer."
+  homepage "https://github.com/brianredbeard/rucat"
+  url "https://github.com/brianredbeard/rucat/archive/refs/tags/v0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 "5a317789196b0527339790b021d7b14d8622c71f54d32a2491b402120e5c1285"
+  license "GPL-3.0-or-later"
+  head "https://github.com/brianredbeard/rucat.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+end
+
--- README.md
+++ README.md
@@ -1,18 +1,1 @@
-# Brianredbeard Rucat
-
-## How do I install these formulae?
-
-`brew install brianredbeard/rucat/<formula>`
-
-Or `brew tap brianredbeard/rucat` and then `brew install <formula>`.
-
-Or, in a `brew bundle` `Brewfile`:
-
-```ruby
-tap "brianredbeard/rucat"
-brew "<formula>"
-```
-
-## Documentation
-
-`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
+# Homebrew rucat
