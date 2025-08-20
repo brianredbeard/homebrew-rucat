5bfc1042585c38f1c2b5b3b89dd8dbadbc9cabb1
Brian 'redbeard' Harrington <redbeard@dead-city.org>
2025-08-19 18:51:08 -0700

    fix: Correctly install rucat man page and shell completions

--- Formula/rucat.rb
+++ Formula/rucat.rb
@@ -10,7 +10,17 @@ class Rucat < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args
+    # First, build the binary from source.
+    system "cargo", "build", "--release", "--locked"
+
+    # Install the binary from the target directory.
+    bin.install "target/release/rucat"
+
+    # Now, install the static assets from the source tree.
+    man1.install "assets/man/rucat.1"
+    bash_completion.install "assets/completions/bash/rucat"
+    fish_completion.install "assets/completions/fish/rucat.fish"
+    zsh_completion.install "assets/completions/zsh/_rucat"
   end
 
   test do
